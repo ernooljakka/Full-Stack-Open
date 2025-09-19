a7e8208fd96ac4ef69a2f1ecfbb085ad1258d9a7
Erno Oljakka <erno.oljakka@gmail.com>
2025-09-19 12:37:20 +0300

    Anekdootit tasks done

--- Osa1/anekdootit/src/App.jsx
+++ Osa1/anekdootit/src/App.jsx
@@ -1,34 +1,56 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState({
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+  });
+
+  const handleClick = () => {
+    setSelected(Math.floor(Math.random() * 8))
+    console.log(selected);
+    
+  }
+
+  const handleVote = () => {
+    setVotes(prev => ({...prev, [selected] : prev[selected] + 1 }))
+
+  }
+
+  const mostVotedKey = Object.keys(votes).reduce(
+    (a, b) => (votes[a] > votes[b] ? a : b)
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <p>has {votes[selected]} votes</p>
+      <br/>
+      <button onClick={handleVote}>Vote</button>
+      <button onClick={handleClick}>Next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[mostVotedKey]}</p>
+      <p>has {votes[mostVotedKey]} votes</p>
+    </div>
   )
 }
 
