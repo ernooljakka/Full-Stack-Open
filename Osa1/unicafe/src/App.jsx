ef8b2e7119d85b2de4ba3527270cdeedb54ad9a2
Erno Oljakka <erno.oljakka@gmail.com>
2025-09-19 10:19:20 +0300

    unicafe tasks done

--- Osa1/unicafe/src/App.jsx
+++ Osa1/unicafe/src/App.jsx
@@ -1,34 +1,75 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const StaticticLine = ({text, value}) => {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Button = ({rating, onClick}) => {
+
+  return (
+    <button onClick={onClick}>
+    {rating}
+    </button>
+  )
+}
+
+const Statistics = ({good, neutral, bad, all}) => {
+
+  return (
+    <table>
+      <tbody>
+        <StaticticLine text="good" value={good} />
+        <StaticticLine text="neutral" value={neutral}/>
+        <StaticticLine text="bad" value={bad} />
+        <StaticticLine text="all" value={good + neutral + bad} />
+        <StaticticLine text="average" value={(good - bad) / all}/>
+        <StaticticLine text="positive" value={good / all * 100 + " %"}/>
+      </tbody>
+    </table>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const goodClickHandler = () => {
+    setGood(good + 1);
+    setAll(all + 1);
+  };
+
+  const neutralClickHandler = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+  };
+  
+  const badClickHandler = () => {
+    setBad(bad + 1);
+    setAll(all + 1);
+  };
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+
+      <Button rating="Good" onClick={goodClickHandler}></Button>
+      <Button rating="Neutral" onClick={neutralClickHandler}></Button>
+      <Button rating="Bad" onClick={badClickHandler}></Button>
+
+      <h2>Statistics</h2>
+
+      {all != 0 ? <Statistics good={good} neutral={neutral} bad={bad} all={all}/> : "No Statistics"}
+
+    </div>
   )
 }
 
