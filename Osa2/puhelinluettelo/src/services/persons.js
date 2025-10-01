e3bc913fef3772efbc5553475b21ddc99b933f1e
Erno Oljakka <erno.oljakka@gmail.com>
2025-10-01 12:53:40 +0300

    Phonebook backend works with phonebook frontend, Task 3.9

--- Osa2/puhelinluettelo/src/services/persons.js
+++ Osa2/puhelinluettelo/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons/'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
@@ -15,12 +15,12 @@ const create = newObject => {
 }
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  const request = axios.put(`${baseUrl}${id}`, newObject)
   return request.then(response => response.data)
 }
 
 const deletePerson = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`)
+  const request = axios.delete(`${baseUrl}${id}`)
   return request.then(response => response.status)
 }
 
--- Osa3/puhelinluettelo_backend/index.js
+++ Osa3/puhelinluettelo_backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 var morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 
@@ -79,13 +82,6 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    if (persons.some(p => p.name === body.name)) {
-        return response.status(400).json({
-            error: "Name must be unique"
-        })
-    }
-
-
     const person = {
         id: generateId(),
         name: body.name,
@@ -97,8 +93,23 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
+app.put('/api/persons/:id', (request, response) => {
 
-const PORT = 3001
+  const body = request.body
+
+  if (persons.some(p => p.name === body.name)) {
+
+    const person = persons.find(p => p.name === body.name)
+
+    person.number = body.number 
+
+    return response.json(person);
+  }
+})
+
+
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
