f0d7de65bfbb429c799a5b606dbddebf220d9061
Erno Oljakka <erno.oljakka@gmail.com>
2025-10-01 15:54:32 +0300

    Another attempt at deploying

--- Osa2/puhelinluettelo/src/services/persons.js
+++ Osa2/puhelinluettelo/src/services/persons.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const baseUrl = import.meta.env.VITE_APP_BACKEND_URL
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
+  const request = axios.get(`${baseUrl}/api/persons`)
 
   return request.then(response => {
     return response.data
@@ -10,17 +10,17 @@ const getAll = () => {
 }
 
 const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
+  const request = axios.post(`${baseUrl}/api/persons`, newObject)
   return request.then(response => response.data)
 }
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}${id}`, newObject)
+  const request = axios.put(`${baseUrl}/api/persons/${id}`, newObject)
   return request.then(response => response.data)
 }
 
 const deletePerson = (id) => {
-  const request = axios.delete(`${baseUrl}${id}`)
+  const request = axios.delete(`${baseUrl}/api/persons/${id}`)
   return request.then(response => response.status)
 }
 
