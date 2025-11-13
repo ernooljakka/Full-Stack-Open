d455623292e764496c842c42780a08b230b0f65a
Erno Oljakka <erno.oljakka@gmail.com>
2025-11-13 09:21:41 +0200

    Patientor base 9.9

--- Osa9/Patientor_backend/index.ts
+++ Osa9/Patientor_backend/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
 const app = express();
 app.use(express.json());
+const cors = require("cors");
 
 const PORT = 3000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.get("/ping", (_req, res) => {
   console.log("someone pinged here");
